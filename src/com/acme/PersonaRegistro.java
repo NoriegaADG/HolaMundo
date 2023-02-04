9a530ee09075c1472c6c200c2ef8dee3cfd8271c
NoriegaADG <34779853+NoriegaADG@users.noreply.github.com>
2023-02-03 18:57:47 -0600

    Comentario sobre codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,7 +3,8 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        System.out.println("Esta instruccion es inservible");
+        //Main comente la linea que considero no es necesaria
+        //System.out.println("Esta instruccion es inservible");
         return "%s %s" .formatted(nombre, apellidos);
 
     }
